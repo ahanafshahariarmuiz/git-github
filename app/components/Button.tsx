1a0d04a16d0ecdbd40b5c7a8877ed6dc97dae32a
Ahnaf Shahriar Muiz <ahanafshahariarmuiz@gmail.com>
2025-10-26 12:36:14 +0600

    transition has been added

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,7 @@
 export default function Button() {
   return (
     <>
-      <button className="bg-gray-50 text-gray-900 py-3 px-5 rounded-md hover:cursor-pointer">Click to handle</button>
+      <button className="bg-gray-50 text-gray-900 py-3 px-5 rounded-md hover:cursor-pointer hover:scale-90 transition-all">Click to handle</button>
     </>
   )
 }
