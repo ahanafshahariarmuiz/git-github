63f34a6fae887b70ea62963addff13016adb9ead
Ahnaf Shahriar Muiz <ahanafshahariarmuiz@gmail.com>
2025-10-26 10:41:10 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div className="w-full flex h-screen">
+      <div className="w-full h-screen bg-amber-400"></div>
+      <div className="w-full h-screen bg-indigo-400"></div>
+    </div>
+  )
+}
