df93a7a6c4672c6ef8cba7beb33c505ca7457f59
Ahnaf Shahriar Muiz <ahanafshahariarmuiz@gmail.com>
2025-10-26 12:25:10 +0600

    button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Button from "./components/Button";
+
 export default function page() {
   return (
     <div className="w-full flex h-screen">
-      <div className="w-full h-screen bg-amber-400"></div>
-      <div className="w-full h-screen bg-indigo-400"></div>
-      <div className="w-full h-screen bg-rose-400"></div>
+      <div className="w-full h-screen bg-amber-400 grid place-content-center">
+        <Button />
+      </div>
+      <div className="w-full h-screen bg-indigo-400 grid place-content-center">
+        <Button />
+      </div>
+      <div className="w-full h-screen bg-rose-400 grid place-content-center">
+        <Button />
+      </div>
     </div>
   )
 }
